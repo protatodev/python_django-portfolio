0afa9033522e7cbead8a8ff794178eebb4d5704c
protatodev <thaddonaghue@yahoo.com>
2018-10-01 21:45:58 -0700

    implement the blog app

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 class Blog(models.Model):
-  title = models.CharField(max_length=50)
+  title = models.CharField(max_length=255)
   image = models.ImageField(upload_to='images/')
-  description = models.CharField(max_length=500)
+  description = models.TextField()
+  pub_date = models.DateTimeField()
 
